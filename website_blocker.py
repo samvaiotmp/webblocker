2dd05359898342404ce64775171178aadc9632da
samvaio <samvaiotmp@gmail.com>
2017-05-14 00:38:43 -0500

    home desktop

---
+++ website_blocker.py
@@ -0,0 +1,9 @@
+import time
+
+hosts_path="/etc/hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com","www.foxnews.com","foxnews.com"]
+
+while True:
+    time.sleep(5)
+    
